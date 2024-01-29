a279fc5cca97157a1e0c2b997f27339aa3c4290d
Pieti Kinnunen <73936530+PietiK@users.noreply.github.com>
2024-01-29 21:58:18 +0200

    Update Board.mjs

--- src/Board.mjs
+++ src/Board.mjs
@@ -8,6 +8,13 @@ export class Board {
   }
 
   toString() {
-    return "TODO";
+    let boardstring = "";
+    for(let i = 0; i<this.height; i++) {
+      for(let j = 0; j<this.width; j++) {
+        boardstring += "."
+      }
+      boardstring += "\n"
+    }
+    return boardstring;
   }
 }
