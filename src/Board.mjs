f2e3053c28f4cbba521ada475972bc7d8aa0e67d
Pieti Kinnunen <73936530+PietiK@users.noreply.github.com>
2024-02-04 15:58:23 +0200

    refactor toString

--- src/Board.mjs
+++ src/Board.mjs
@@ -11,11 +11,11 @@ export class Board {
 
   toString() {
     let boardstring = "";
-    for(let i = 0; i<this.height; i++) {
-      for(let j = 0; j<this.width; j++) {
-        boardstring += "."
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        boardstring += this.cells[i][j];
       }
-      boardstring += "\n"
+      boardstring += "\n";
     }
     return boardstring;
   }
