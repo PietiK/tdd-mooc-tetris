133455c8231c3d136f5af688a4d008b405d85410
Pieti Kinnunen <73936530+PietiK@users.noreply.github.com>
2024-02-04 15:57:30 +0200

    Add cells

--- src/Board.mjs
+++ src/Board.mjs
@@ -1,10 +1,12 @@
 export class Board {
   width;
   height;
+  cells;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.cells = new Array(height).fill(null).map(() => new Array(width).fill("."));
   }
 
   toString() {
