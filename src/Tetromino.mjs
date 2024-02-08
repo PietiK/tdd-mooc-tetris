3c176959ab97852487d4e9d40acda2837f7412a0
Pieti Kinnunen <73936530+PietiK@users.noreply.github.com>
2024-02-08 20:40:28 +0200

    feat: initial Tetromino, T_SHAPE

---
+++ src/Tetromino.mjs
@@ -0,0 +1,15 @@
+import { RotatingShape } from "./RotatingShape.mjs";
+
+export class Tetromino extends RotatingShape {
+  constructor(shape) {
+    super(shape);
+  }
+
+  static get T_SHAPE() {
+    return new Tetromino(
+      `.T.
+       TTT
+       ...`
+    );
+  }
+}
